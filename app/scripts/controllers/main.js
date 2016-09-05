f4fd2a02a604bdad9f097f62964edd16019dabd4
yaoyuan410203 <yaoyuan4102@gmail.com>
2016-09-04 23:38:50 -0500

    navigation bar

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stockDogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stockDogApp
+ */
+angular.module('stockDogApp')
+  .controller('MainCtrl', function ($scope, $location, WatchlistService) {
+    $scope.watchlists = WatchlistService.query();
+
+    $scope.$watch(function () {
+        return $location.path();
+    }, function (path) {
+        if (_.contains(path, 'watchlist')) {
+            $scope.activeView = 'watchlist';
+        } else if (_.contains(path, 'dashboard')) {
+            $scope.activeView = 'dashboard';
+        }
+    })
+  });
